0c1212bcb9f9c443bac47895d57b77c67252a507
herkommer <m@herkommer.co.uk>
2021-10-08 11:33:01 +0200

    ready to go

---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const port = 8042;
+const indexRouter = require('./routes/index');
+
+app.use(express.static('public'));
+
+app.use('/', indexRouter);
+
+app.listen(port);
+
+console.log('Server up and running, listning on port: ' + port);
